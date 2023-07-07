e8b642023c69cf2ff3b9fc400d40cee370d83a4a
19h <kenan@sly.mn>
2023-07-07 05:27:55 +0200

    evilcorp secondpilot

--- src/providers/openai/api.ts
+++ src/providers/openai/api.ts
@@ -1,32 +1,50 @@
+import fs from 'fs';
+
 export interface OpenAIFetchPayload {
-  apiKey: string
-  baseUrl: string
-  body: Record<string, any>
-  signal?: AbortSignal
+    apiKey: string;
+    baseUrl: string;
+    body: Record<string, any>;
+    signal?: AbortSignal;
 }
 
-export const fetchChatCompletion = async(payload: OpenAIFetchPayload) => {
-  const initOptions = {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${payload.apiKey}`,
-    },
-    method: 'POST',
-    body: JSON.stringify(payload.body),
-    signal: payload.signal,
-  }
-  return fetch(`${payload.baseUrl}/v1/chat/completions`, initOptions)
-}
+export const fetchChatCompletion = async (payload: OpenAIFetchPayload) => {
+    const res = await fetch('https://api.github.com/copilot_internal/v2/token', {
+        'method': 'GET',
+        'headers': {
+            'Authorization': 'token ' + process.env.GH_TOKEN,
+            'User-Agent': 'GithubCopilot/1.86.92',
+        },
+    });
 
-export const fetchImageGeneration = async(payload: OpenAIFetchPayload) => {
-  const initOptions = {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${payload.apiKey}`,
-    },
-    method: 'POST',
-    body: JSON.stringify(payload.body),
-    signal: payload.signal,
-  }
-  return fetch(`${payload.baseUrl}/v1/images/generations`, initOptions)
-}
+    const json = await res.json();
+
+    const cat_token = json.token;
+
+    payload.body.model = 'copilot-chat';
+
+    return fetch(
+        'https://copilot-proxy.githubusercontent.com/v1/chat/completions',
+        {
+            'method': 'POST',
+            'headers': {
+                'Authorization': `Bearer ${cat_token}`,
+                'Content-Type': 'application/json',
+            },
+            'body': JSON.stringify(payload.body),
+            signal: payload.signal,
+        },
+    );
+};
+
+export const fetchImageGeneration = async (payload: OpenAIFetchPayload) => {
+    const initOptions = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${payload.apiKey}`,
+        },
+        method: 'POST',
+        body: JSON.stringify(payload.body),
+        signal: payload.signal,
+    };
+    return fetch(`${payload.baseUrl}/v1/images/generations`, initOptions);
+};
